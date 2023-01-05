451199a1c0ad29a3e02f412210bdd3d481a2a566
JosieMCantu <josiex2@gmail.com>
2023-01-05 15:52:51 -0800

    skeleton plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as esbuild from 'esbuild-wasm';
 import { createRoot } from 'react-dom/client';
 import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-const App = () => {
+function App(): JSX.Element {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
     const ref = useRef<any>();
@@ -22,21 +23,31 @@ const App = () => {
         if (!ref.current) {
             return;
         }
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
         });
-        setCode(result.code);
+        setCode(result.outputFiles[0].text);
     };
 
+    const css = `
+    textarea {
+        height: 300px;
+        width: 400px;
+        background-color: pink;
+    }`
+
     return <div>
+        <style>{css}</style>
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
         <div>
             <button onClick={onClick}>Push Me</button>
         </div>
         <pre>{code}</pre>
-    </div>
-};
+    </div>;
+}
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
